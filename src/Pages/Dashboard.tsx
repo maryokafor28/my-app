5d7eee06472990fb177f5b71a3dc52e3b02c9e4d
maryokafor28 <vincyokafor@gmail.com>
2025-03-16 20:27:55 +0000

    styling

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from '../context/AuthContext';
+import '../styles/Dashboard.css'; // Import the CSS file
 
 const Dashboard = () => {
   const { user } = useAuth();
@@ -6,11 +7,13 @@ const Dashboard = () => {
   if (!user) return null;
 
   return (
-    <div>
-      <h2>Dashboard</h2>
-      {user.role === 'Admin' && <p>Admin Controls</p>}
-      {user.role === 'Editor' && <p>Content Editor Panel</p>}
-      {user.role === 'Viewer' && <p>Read-Only Reports</p>}
+    <div className="dashboard-container">
+      <div>
+        <h2 className='dashboard'>Dashboard</h2>
+        {user.role === 'Admin' && <p>Admin Controls</p>}
+        {user.role === 'Editor' && <p>Content Editor Panel</p>}
+        {user.role === 'Viewer' && <p>Read-Only Reports</p>}
+      </div>
     </div>
   );
 };
